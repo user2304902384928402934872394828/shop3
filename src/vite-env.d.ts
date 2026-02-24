74bfa992ad57a1cfe14e70e600715af5e2b08cae
user2304902384928402934872394828 <jameswently97@gmail.com>
2026-02-23 23:18:46 -0600

    asd

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // adjust if deploying to subfolder
+  base: '/App.tsx', // adjust if deploying to subfolder
 });
